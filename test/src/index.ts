b647bcce5824bc318d1722083d975cb0b43478c3
Dishant1804 <dishantmiyani1804@gmail.com>
2024-08-23 22:55:50 +0530

    cleaned frontend

--- test/src/index.ts
+++ test/src/index.ts
@@ -1,10 +1,4 @@
-import {
-    Account,
-    Aptos,
-    AptosConfig,
-    Network,
-    SigningSchemeInput,
-} from "@aptos-labs/ts-sdk";
+import { Account, Aptos, AptosConfig, Network, SigningSchemeInput } from "@aptos-labs/ts-sdk";
 
 const account1 = Account.generate(); // defaults to Legacy Ed25519
 // const account2 = Account.generate({ scheme: SigningSchemeInput.Secp256k1 }); // Single Sender Secp256k1
@@ -12,7 +6,6 @@ const account3 = Account.generate({
   scheme: SigningSchemeInput.Ed25519,
   legacy: false,
 }); // Single Sender Ed25519
-
 
 console.log(account1.publicKey);
 console.log(account1.privateKey);
