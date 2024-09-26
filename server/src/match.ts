ee0dea4f0441c5c0c4e84dd5222d8ce7477c976b
Krish Soni <sonikrish994@gmail.com>
2024-09-26 23:14:30 +0530

    Refactor: Update Dockerfile and WebSocket port

--- server/src/match.ts
+++ server/src/match.ts
@@ -20,7 +20,7 @@ type MatchState = {
 };
 
 // Set up a WebSocket server
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 343 });
 
 // Simulated cricket match data
 let matchState: MatchState = {
